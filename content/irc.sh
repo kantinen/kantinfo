e827e6076239744f4708e3d871cd15c21bed93cb
Niels G. W. Serup <ngws@metanohi.name>
2015-03-25 16:04:52 +0100

    regnbue!

--- content/irc.sh
+++ content/irc.sh
@@ -2,5 +2,6 @@
 
 irc_out=$HOME/diku_irc_out
 
-echo 'Kom på IRC på #diku på irc.freenode.net!  Spørg din nabo om hjælp.'
+echo 'Kom på IRC på #diku på irc.freenode.net!  Spørg din nabo om hjælp.' | toilet -f term --gay
+
 tail -f -n 10 $irc_out
