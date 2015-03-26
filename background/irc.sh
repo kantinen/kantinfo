b72a420fdc231e0cea0e54f0ab206db913cdc4e8
Niels G. W. Serup <ngws@metanohi.name>
2015-03-26 17:26:57 +0100

    Fiksede IRC med Andreas.

--- background/irc.sh
+++ background/irc.sh
@@ -1,11 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
-trap 'killall sic; exit' INT QUIT TERM EXIT
+trap 'killall -s 9 sic; exit' INT QUIT TERM EXIT
 
 irc_out=/tmp/diku_irc_out
 
+usercolor='\e[0;32m'
+msgcolor='\e[0;37m'
+
+color_usermsg() {
+    # Første linje af fmt-uddataen.
+    read line
+    start=$(echo "$line" | cut -d '>' -f 1)
+    end=$(echo "$line" | cut -d '>' -f 2-)
+    {
+	echo -en "$usercolor"
+	echo -n "$start>"
+	echo -en "$msgcolor"
+	echo "$end"
+    }
+
+    cat # Resterende linjer.
+}
+
+ircloop() {
+    while true; do
+	sic -h irc.freenode.net -n infoskaerm
+    done
+}
+
+# Input til IRC-klienten.
 in=$(mktemp)
 touch $in
-(tail -f $in | sic -h irc.freenode.net -n infoskaerm | grep --line-buffered -E '^\#diku' | sed -ur 's/[^<]+(.+)/\1/' > $irc_out) &
-echo ':j #diku' > $in
+
+# Kør klienten i baggrunden.
+{
+    tail -f $in \
+	| ircloop \
+	| grep --line-buffered -E '^\#hongabar' \
+	| sed -ur 's/[^<]+(.+)/\1/' \
+	| while IFS='' read line; do
+	echo "$line" | fmt -75 | color_usermsg >> $irc_out
+    done
+
+} &
+
+# Join #diku.
+echo ':j #hongabar' > $in
+
+# Wait for the client to finish.
 wait
--- content/irc.sh
+++ content/irc.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/bin/bash
 
 irc_out=/tmp/diku_irc_out
 
-velkommen() {
+top() {
     tput cup 0 0
     echo 'Kom på IRC på #diku på irc.freenode.net!  Spørg din nabo om hjælp.' | toilet -f term --gay
 }
 
-tail -f $irc_out | while read line; do
-    velkommen
-    tail -n 16 $irc_out
+bund() {
+    tput cup 21 0
+    echo -n $(echo 'Du kan f.eks. logge ind her: https://webchat.freenode.net/?channels=#diku' | toilet -f term --gay)
+}
+
+top
+tail -n 1 -f $irc_out | while read line; do
+    clear
+    top
+    tail -n 20 $irc_out
+    bund
 done
