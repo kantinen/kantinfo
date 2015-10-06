e19d346ad2bf60fdeefd321dbc1e67d527dc740d
Niels G. W. Serup <ngws@metanohi.name>
2015-10-06 18:50:26 +0200

    Bedre officiel kontakt-email-adresse.

--- infoscreen.py
+++ infoscreen.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright © 2014-2015 Infoskærms-gruppen <infoskaerm@kantinen.org>
+# Copyright © 2014-2015 Infoskærms-gruppen <infoskaerm@dikumail.dk>
 #
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
