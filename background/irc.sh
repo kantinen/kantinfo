eff87f9a3cd774fd5ff36554d94e24f478d7aa8e
Niels G. W. Serup <ngws@metanohi.name>
2015-10-06 20:12:36 +0200

    Hov, brug den rigtige adresse.

--- background/irc.sh
+++ background/irc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright © 2014-2015 Infoskærms-gruppen <infoskaerm@kantinen.org>
+# Copyright © 2014-2015 Infoskærms-gruppen <infoskaerm@dikumail.dk>
 #
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
