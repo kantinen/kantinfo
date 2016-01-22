ca879653730188ec1b4d0b53ab9ccd7dbbb141b4
Niels G. W. Serup <ngws@metanohi.name>
2016-01-22 18:18:00 +0100

    mere generelet play-video script

--- scripts/play-video.sh
+++ scripts/play-video.sh
@@ -9,6 +9,5 @@ trap 'xdotool mousemove 2000 2000' INT QUIT TERM EXIT
 
 # Actually play the file, but use an old SDL backend, since ODroids are silly.
 video_path="$1"
-start_at="$2"
-end_at="$3"
-mplayer -really-quiet $start_at $end_at "$video_path" -fs -vo sdl
+shift
+mplayer -really-quiet "$@" "$video_path" -fs -vo sdl
