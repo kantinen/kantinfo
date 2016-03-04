3abe44619e409004cc6f2c60de0a723f90ca66b5
Niels G. W. Serup <ngws@metanohi.name>
2016-03-04 22:53:30 +0100

    Refactor kantinfo.py.

--- kantinfo.py
+++ kantinfo.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
-# Copyright © 2014-2015 Infoskærms-gruppen <infoskaerm@dikumail.dk>
+# Copyright © 2014-2016 Infoskærms-gruppen <infoskaerm@dikumail.dk>
 #
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
 
-# This is the control script for the informational monitors in the DIKU canteen,
-# but it can be used otherwhere as well.  See the README.md file to find out how
-# to use it.
+# This is the control script for the informational monitors in the DIKU
+# canteen, but it can be (and is) used in other places as well.  See the
+# README.md file to find out how to use it.
 
 
 import sys
@@ -16,11 +16,11 @@ import os
 import signal
 import time
 import subprocess
-import yaml
 import random
 import re
 import tempfile
 import traceback
+import yaml
 
 
 globs = {
@@ -95,17 +95,21 @@ def find_next(old_selection, old_list, new_list):
     if i is not None:
         # Find the first element following old_list[i] that is also in
         # new_list.
-        next = find_or_none(lambda x: x in new_list, rotate(i, old_list))
-        if next is not None:
-            return next
+        next_element = find_or_none(lambda x: x in new_list, rotate(i, old_list))
+        if next_element is not None:
+            return next_element
 
     # old_selection was not even in old_list.
     if len(new_list) == 0:
-      return None
+        return None
     else:
-      return new_list[0]
+        return new_list[0]
 
 def find_next_content(old_selection, old_content):
+    '''
+    Find the next piece of content (assuming a circular walkthrough),
+    and the list of elements that should come after that.
+    '''
     paths = [os.path.join(globs['content_directory'], f)
              for f in os.listdir(globs['content_directory'])]
     new_content = [p for p in paths
@@ -113,7 +117,21 @@ def find_next_content(old_selection, old_content):
                    not p.endswith(globs['config_ending'])]
     return (find_next(old_selection, old_content, new_content), new_content)
 
-def run_program(progname, args):
+def _time_to_min(t):
+    if isinstance(t, int):
+        return t
+    else:
+        h, m = map(int, t.split(':'))
+        return h * 60 + m
+
+def _time_to_sec(t):
+    if isinstance(t, int):
+        return t
+    else:
+        m, s = map(int, str(t).split(':'))
+        return m * 60 + s
+
+def _run_program(progname, args):
     print('Runs {} with arguments {}'.format(progname, args))
     proc = subprocess.Popen([progname] + args,
                             stdout=None,
@@ -123,28 +141,27 @@ def run_program(progname, args):
                             preexec_fn=os.setsid)
     return proc
 
-
-def play_video(path):
+def _play_video(path):
     video_cache_dir = os.path.expanduser('~/.kantinfo-video-cache')
     start_pos = []
-    end_pos= []
+    end_pos = []
     try:
         intervals = globs['current_conf']['intervals']
-        [raw_start_pos, raw_end_pos] = random.sample(intervals, 1)[0]
-        start_in_seconds = time_to_sec(raw_start_pos)
-        end_in_seconds = time_to_sec(raw_end_pos) - start_in_seconds
+        raw_start_pos, raw_end_pos = random.sample(intervals, 1)[0]
+        start_in_seconds = _time_to_sec(raw_start_pos)
+        end_in_seconds = _time_to_sec(raw_end_pos) - start_in_seconds
         start_pos = ['-ss', str(start_in_seconds)]
         end_pos = ['-endpos', str(end_in_seconds)]
     except (TypeError, KeyError):
         try:
             raw_start_pos = globs['current_conf']['start_pos']
-            start_in_seconds = time_to_sec(raw_start_pos)
+            start_in_seconds = _time_to_sec(raw_start_pos)
             start_pos = ['-ss', str(start_in_seconds)]
         except (TypeError, KeyError):
             pass
         try:
             raw_end_pos = globs['current_conf']['end_pos']
-            end_in_seconds = time_to_sec(raw_end_pos)
+            end_in_seconds = _time_to_sec(raw_end_pos)
             end_pos = ['-endpos', str(end_in_seconds - start_in_seconds)]
         except (TypeError, KeyError):
             pass
@@ -166,44 +183,44 @@ def play_video(path):
     else:
         video_path = os.path.expanduser(path)
 
-    return run_program(os.path.join(base_dir, 'scripts/play-video.sh'),
+    return _run_program(os.path.join(base_dir, 'scripts/play-video.sh'),
                        [video_path] + start_pos + end_pos)
 
-def show_url_in_browser(url):
-    return run_program('surf',
+def _show_url_in_browser(url):
+    return _run_program('surf',
                        ['-p', # Disable plugins.
                         '-b', # Disable scrollbars.
                         '-F', # Run in fullscreen.
                         url])
 
-def url_handler(url):
+def _url_handler(url):
     if url.endswith('.mkv') or url.endswith('.webm') or url.endswith('.mp4') \
        or url.endswith('.avi') or url.endswith('.mpg') or url.endswith('.ogv') \
        or url.startswith('http://youtube.com/') or url.startswith('https://youtube.com/') \
        or url.startswith('http://www.youtube.com/') or url.startswith('https://www.youtube.com/') \
        or url.startswith('http://youtu.be/') or url.startswith('https://youtu.be/') \
        or url.startswith('http://imgur.com/'):
-        return play_video
+        return _play_video
     else:
-        return show_url_in_browser
+        return _show_url_in_browser
 
-def open_url(urlfile):
+def _open_url(urlfile):
     with open(urlfile) as f:
         url = next(filter(lambda s: not s.startswith('#'),
                           f.read().strip().split('\n')))
-    return url_handler(url)(url)
+    return _url_handler(url)(url)
 
-def show_in_browser(filename):
-    return show_url_in_browser('file://' + os.path.join(os.getcwd(), filename))
+def _show_in_browser(filename):
+    return _show_url_in_browser('file://' + os.path.join(os.getcwd(), filename))
 
-def show_image(filename):
-    return run_program('feh',
-                       ['-F', '-Z', os.path.join(os.getcwd(), filename)])
+def _show_image(filename):
+    return _run_program('feh',
+                        ['-F', '-Z', os.path.join(os.getcwd(), filename)])
 
-def eval_program(filename):
+def _eval_program(filename):
     try:
-        _, typ, eval = filename.rsplit('.', 2)
-        assert eval == 'eval'
+        _, typ, eval_ending = filename.rsplit('.', 2)
+        assert eval_ending == 'eval'
     except (ValueError, AssertionError):
         print('.eval script is not proper .eval script: {}'.format(filename))
 
@@ -213,42 +230,28 @@ def eval_program(filename):
             f.write(content)
         return show_content(tfile.name)
 
-def run_in_terminal(filename):
-    return run_program('lxterminal',
-                       ['-e', # Start program
-                        os.path.join(os.getcwd(), filename)])
+def _run_in_terminal(filename):
+    return _run_program('lxterminal',
+                        ['-e', # Start program
+                         os.path.join(os.getcwd(), filename)])
 
 def show_content(filename):
     print('Attempting to show {}'.format(filename))
     extension = os.path.splitext(filename)[1][1:]
     try:
         f = {
-            'html': lambda: show_in_browser(filename),
-            'jpg': lambda: show_image(filename),
-            'png': lambda: show_image(filename),
-            'gif': lambda: show_in_browser(filename),
-            'url': lambda: open_url(filename),
-            'sh': lambda: run_program(filename, []),
-            'eval': lambda: eval_program(filename),
-            'terminal': lambda: run_in_terminal(filename)
+            'html': lambda: _show_in_browser(filename),
+            'jpg': lambda: _show_image(filename),
+            'png': lambda: _show_image(filename),
+            'gif': lambda: _show_in_browser(filename),
+            'url': lambda: _open_url(filename),
+            'sh': lambda: _run_program(filename, []),
+            'eval': lambda: _eval_program(filename),
+            'terminal': lambda: _run_in_terminal(filename)
         }[extension]
     except KeyError:
         raise Exception('I have no idea how to show a {} file.'.format(extension))
     return f()
-
-def time_to_min(t):
-    if isinstance(t, int):
-        return t
-    else:
-        h, m = map(int, t.split(':'))
-        return h * 60 + m
-
-def time_to_sec(t):
-    if isinstance(t, int):
-        return t
-    else:
-        m, s = map(int, str(t).split(':'))
-        return m * 60 + s
 
 def infoscreen(do_random=False):
     '''
@@ -272,7 +275,6 @@ def infoscreen(do_random=False):
             with open(content_conf) as f:
                 conf = f.read()
             conf = yaml.load(conf)
-            conf.__getitem__
             globs['current_conf'] = conf
         except (IOError, yaml.YAMLError, AttributeError):
             pass
@@ -281,7 +283,7 @@ def infoscreen(do_random=False):
                 dur = conf['duration']
             except (TypeError, KeyError):
                 pass
-        # An end-point will always overrule duration
+            # An end point will always overrule duration.
             if 'end_pos' in conf or 'intervals' in conf:
                 dur = -1
             try:
@@ -301,8 +303,8 @@ def infoscreen(do_random=False):
             except (TypeError, KeyError):
                 pass
             else:
-                start_at = time_to_min(start_at)
-                end_at = time_to_min(end_at)
+                start_at = _time_to_min(start_at)
+                end_at = _time_to_min(end_at)
                 tloc = time.localtime()
                 now = tloc.tm_hour * 60 + tloc.tm_min
                 if start_at < end_at:
@@ -363,18 +365,21 @@ def infoscreen(do_random=False):
                 proc_prev = proc
 
 def run_infoscreen(args):
+    '''
+    Run the infoscreen based on the list of string arguments in `args`.
+    '''
     do_random = False
     if '--help' in args:
-        print_help()
+        _print_help()
         sys.exit(0)
     elif '--random' in args:
         do_random = True
         args.remove('--random')
-        
+
     try:
         globs['content_directory'] = args[0]
     except IndexError:
-        print_help()
+        _print_help()
         sys.exit(0)
 
     try:
@@ -403,7 +408,7 @@ def run_infoscreen(args):
         except KeyboardInterrupt:
             pass
 
-def print_help():
+def _print_help():
     print('kantinfo from git; see README.md for instructions')
     print()
     print('Usage:')
@@ -412,7 +417,6 @@ def print_help():
     print('Options:')
     print('  --help    Print this text.')
     print('  --random  Show the slides in random order.')
-                
+
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    run_infoscreen(args)
+    run_infoscreen(sys.argv[1:])
